a2b8b613052a7abecf6d1fbf311cf9392e0ab825
Manel J <manel.jimeno@gmail.com>
2022-06-06 23:58:53 +0200

    Add unit tests

---
+++ src/ut/u_test_1.cpp
@@ -0,0 +1,28 @@
+/**
+ * simcom u_test_1.cpp
+ *
+ * Copyright (C) 2022, Manel J
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "version.h"
+#include <gtest/gtest.h>
+
+// NOLINTNEXTLINE
+TEST(Version, check)
+{
+    auto version = simcom::package::getVersion();
+    EXPECT_EQ(version.m_version.empty(), false);
+}
